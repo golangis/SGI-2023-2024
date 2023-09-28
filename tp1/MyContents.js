013c542e9b012142a656978cf9d7b1c8176617a2
up202007928 <up202007928@fe.up.pt>
2023-09-28 16:11:28 +0100

    format code for readibility

--- tp1/MyContents.js
+++ tp1/MyContents.js
@@ -1,50 +1,60 @@
-import * as THREE from 'three';
-import { MyAxis } from './MyAxis.js';
+import * as THREE from "three";
+import { MyAxis } from "./MyAxis.js";
 
 /**
  *  This class contains the contents of out application
  */
-class MyContents  {
-
+class MyContents {
     /**
        constructs the object
        @param {MyApp} app The application object
-    */ 
+    */
     constructor(app) {
-        this.app = app
-        this.axis = null
+        this.app = app;
+        this.axis = null;
 
         // box related attributes
-        this.boxMesh = null
-        this.boxMeshSize = 1.0
-        this.boxEnabled = true
-        this.lastBoxEnabled = null
-        this.boxDisplacement = new THREE.Vector3(0,2,0)
+        this.boxMesh = null;
+        this.boxMeshSize = 1.0;
+        this.boxEnabled = true;
+        this.lastBoxEnabled = null;
+        this.boxDisplacement = new THREE.Vector3(0, 2, 0);
 
         // plane related attributes
-        this.diffusePlaneColor = "#00ffff"
-        this.specularPlaneColor = "#777777"
-        this.planeShininess = 30
-        this.planeMaterial = new THREE.MeshPhongMaterial({ color: this.diffusePlaneColor, 
-            specular: this.diffusePlaneColor, emissive: "#000000", shininess: this.planeShininess })
+        this.diffusePlaneColor = "#00ffff";
+        this.specularPlaneColor = "#777777";
+        this.planeShininess = 30;
+        this.planeMaterial = new THREE.MeshPhongMaterial({
+            color: this.diffusePlaneColor,
+            specular: this.diffusePlaneColor,
+            emissive: "#000000",
+            shininess: this.planeShininess,
+        });
     }
 
     /**
      * builds the box mesh with material assigned
      */
-    buildBox() {    
-        let boxMaterial = new THREE.MeshPhongMaterial({ color: "#ffff77", 
-        specular: "#000000", emissive: "#000000", shininess: 90 })
+    buildBox() {
+        let boxMaterial = new THREE.MeshPhongMaterial({
+            color: "#ffff77",
+            specular: "#000000",
+            emissive: "#000000",
+            shininess: 90,
+        });
 
         // Create a Cube Mesh with basic material
-        let box = new THREE.BoxGeometry(  this.boxMeshSize,  this.boxMeshSize,  this.boxMeshSize );
-        this.boxMesh = new THREE.Mesh( box, boxMaterial );
+        let box = new THREE.BoxGeometry(
+            this.boxMeshSize,
+            this.boxMeshSize,
+            this.boxMeshSize
+        );
+        this.boxMesh = new THREE.Mesh(box, boxMaterial);
         this.boxMesh.rotation.x = -Math.PI / 2;
         this.boxMesh.position.y = this.boxDisplacement.y;
 
-        
         // Geometric transformations
-        
+
         // Rotate in X axis + Translate in Z axis
         // this.boxMesh.rotation.x = Math.PI / 9;
         // this.boxDisplacement.z = 3;
@@ -52,35 +62,36 @@ class MyContents  {
         // Scaling then Translate
         // this.boxMesh.scale.set(1,1,2);
         // this.boxDisplacement.x = 3;
-        
     }
 
     /**
      * initializes the contents
      */
     init() {
-       
-        // create once 
+        // create once
         if (this.axis === null) {
             // create and attach the axis to the scene
-            this.axis = new MyAxis(this)
-            this.app.scene.add(this.axis)
+            this.axis = new MyAxis(this);
+            this.app.scene.add(this.axis);
         }
 
         // add a point light on top of the model
-        const pointLight = new THREE.PointLight( 0xffffff, 500, 0 );
-        pointLight.position.set( 0, 20, 0 );
-        this.app.scene.add( pointLight );
+        const pointLight = new THREE.PointLight(0xffffff, 500, 0);
+        pointLight.position.set(0, 20, 0);
+        this.app.scene.add(pointLight);
 
         // add a point light helper for the previous point light
         const sphereSize = 0.5;
-        const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
-        this.app.scene.add( pointLightHelper );
+        const pointLightHelper = new THREE.PointLightHelper(
+            pointLight,
+            sphereSize
+        );
+        this.app.scene.add(pointLightHelper);
 
         // add an ambient light
-        const ambientLight = new THREE.AmbientLight( 0x555555 );
-        this.app.scene.add( ambientLight );
-        
+        const ambientLight = new THREE.AmbientLight(0x555555);
+        this.app.scene.add(ambientLight);
+
         this.buildRoom();
 
         this.buildTable();
@@ -90,7 +101,7 @@ class MyContents  {
         this.buildCake();
 
         this.buildCandle();
-        
+
         /*
         this.buildBox()
         
@@ -170,197 +181,211 @@ class MyContents  {
         */
     }
 
-
     /**
      * Creates the walls and floor of the room
      */
 
-    buildRoom(){
-                
+    buildRoom() {
         // Create floor Mesh with basic material
-        
-        let floor = new THREE.PlaneGeometry( 10, 10 );
-        this.floorMesh = new THREE.Mesh( floor, this.planeMaterial );
+
+        let floor = new THREE.PlaneGeometry(10, 10);
+        this.floorMesh = new THREE.Mesh(floor, this.planeMaterial);
         this.floorMesh.rotation.x = -Math.PI / 2;
         this.floorMesh.position.y = -0;
 
-        
         // Create walls
-        
-        let leftWall = new THREE.PlaneGeometry( 10, 5 );
-        this.leftWallMesh = new THREE.Mesh( leftWall, this.planeMaterial );
+
+        let leftWall = new THREE.PlaneGeometry(10, 5);
+        this.leftWallMesh = new THREE.Mesh(leftWall, this.planeMaterial);
         this.leftWallMesh.rotation.x = Math.PI;
         this.leftWallMesh.position.set(0, 2.5, 5);
-        
-        let rightWall = new THREE.PlaneGeometry( 10, 5 );
-        this.rightWallMesh = new THREE.Mesh( rightWall, this.planeMaterial );
-        this.rightWallMesh.rotation.x = 2*Math.PI;
+
+        let rightWall = new THREE.PlaneGeometry(10, 5);
+        this.rightWallMesh = new THREE.Mesh(rightWall, this.planeMaterial);
+        this.rightWallMesh.rotation.x = 2 * Math.PI;
         this.rightWallMesh.position.set(0, 2.5, -5);
-        
-        let backWall = new THREE.PlaneGeometry( 10,5 );
-        this.backWallMesh = new THREE.Mesh( backWall, this.planeMaterial );
-        this.backWallMesh.rotation.y = Math.PI/2;
+
+        let backWall = new THREE.PlaneGeometry(10, 5);
+        this.backWallMesh = new THREE.Mesh(backWall, this.planeMaterial);
+        this.backWallMesh.rotation.y = Math.PI / 2;
         this.backWallMesh.position.set(-5, 2.5, 0);
 
         this.roomGroup = new THREE.Group();
 
-        this.roomGroup.add(this.floorMesh, this.leftWallMesh, this.rightWallMesh, this.backWallMesh)
+        this.roomGroup.add(
+            this.floorMesh,
+            this.leftWallMesh,
+            this.rightWallMesh,
+            this.backWallMesh
+        );
 
-        this.app.scene.add(this.roomGroup)
-        
+        this.app.scene.add(this.roomGroup);
     }
-    
 
     /**
      * Create table
      */
-    buildTable(){
-        
+    buildTable() {
         // Create table top
-        
+
         this.tableGroup = new THREE.Group();
 
-        let tableMaterial = new THREE.MeshPhongMaterial({ color: "#995e17", 
-        specular: "#000000", emissive: "#000000", shininess: 90 })
-        let tableTop = new THREE.BoxGeometry(3,2,0.2);
-        this.tableMesh = new THREE.Mesh( tableTop, tableMaterial );
-        
-        this.tableGroup.position.y = 2
+        let tableMaterial = new THREE.MeshPhongMaterial({
+            color: "#995e17",
+            specular: "#000000",
+            emissive: "#000000",
+            shininess: 90,
+        });
+        let tableTop = new THREE.BoxGeometry(3, 2, 0.2);
+        this.tableMesh = new THREE.Mesh(tableTop, tableMaterial);
 
-        
+        this.tableGroup.position.y = 2;
+
         // Create table legs
-        
-        let tableLeg = new THREE.CylinderGeometry(0.1, 0.1, 2)
-        this.tableLegMesh = new THREE.Mesh(tableLeg, tableMaterial)
-        
-        for (let j = -1; j < 2; j+=2) {
-            for(let i = -1; i < 2; i+=2){
+
+        let tableLeg = new THREE.CylinderGeometry(0.1, 0.1, 2);
+        this.tableLegMesh = new THREE.Mesh(tableLeg, tableMaterial);
+
+        for (let j = -1; j < 2; j += 2) {
+            for (let i = -1; i < 2; i += 2) {
                 let instance = this.tableLegMesh.clone();
-                instance.position.set((i)*1.3, (j)*0.8, -1);
-                instance.rotateX(-Math.PI/2)
+                instance.position.set(i * 1.3, j * 0.8, -1);
+                instance.rotateX(-Math.PI / 2);
                 this.tableMesh.add(instance);
             }
         }
-        
-        this.tableGroup.rotateX(-Math.PI/2)
-        
-        this.tableGroup.add(this.tableMesh)
-        this.roomGroup.add(this.tableGroup)
 
+        this.tableGroup.rotateX(-Math.PI / 2);
+
+        this.tableGroup.add(this.tableMesh);
+        this.roomGroup.add(this.tableGroup);
     }
-    
 
     buildCakeStand() {
-        
-        let plateMaterial = new THREE.MeshPhongMaterial({ color: "#e8faf2", 
-            specular: "#000000", emissive: "#000000", shininess: 90
-        })
-        
-		// Create cake stand base
-		let cakeStand = new THREE.CylinderGeometry(0.1, 0.22, 0.2);
-		this.cakeStandMesh = new THREE.Mesh(cakeStand, plateMaterial);
-        this.cakeStandMesh.position.set(0, 0, 0.2);
-        
-        // Create cake stand plate
-		let cakeStandPlate = new THREE.CylinderGeometry(0.35, 0.32, 0.02);
-		this.cakeStandPlateMesh = new THREE.Mesh(cakeStandPlate, plateMaterial);
-		this.cakeStandPlateMesh.position.set(0, 0.1, 0);
-        
+        let plateMaterial = new THREE.MeshPhongMaterial({
+            color: "#e8faf2",
+            specular: "#000000",
+            emissive: "#000000",
+            shininess: 90,
+        });
 
-        this.cakeStandMesh.rotateX(Math.PI/2)
-        this.tableMesh.add(this.cakeStandMesh)
+        // Create cake stand base
+        let cakeStand = new THREE.CylinderGeometry(0.1, 0.22, 0.2);
+        this.cakeStandMesh = new THREE.Mesh(cakeStand, plateMaterial);
+        this.cakeStandMesh.position.set(0, 0, 0.2);
+
+        // Create cake stand plate
+        let cakeStandPlate = new THREE.CylinderGeometry(0.35, 0.32, 0.02);
+        this.cakeStandPlateMesh = new THREE.Mesh(cakeStandPlate, plateMaterial);
+        this.cakeStandPlateMesh.position.set(0, 0.1, 0);
+
+        this.cakeStandMesh.rotateX(Math.PI / 2);
+        this.tableMesh.add(this.cakeStandMesh);
         this.cakeStandMesh.add(this.cakeStandPlateMesh);
     }
-    
+
     buildCake() {
+        let cakeExteriorMaterial = new THREE.MeshPhongMaterial({
+            color: "#eb8f9b",
+            specular: "#000000",
+            emissive: "#000000",
+            shininess: 90,
+        });
 
-        let cakeExteriorMaterial = new THREE.MeshPhongMaterial({ color: "#eb8f9b", 
-            specular: "#000000", emissive: "#000000", shininess: 90
-        })
-
-        let cakeInteriorMaterial = new THREE.MeshBasicMaterial({ color: "#d6b383"})
+        let cakeInteriorMaterial = new THREE.MeshBasicMaterial({
+            color: "#d6b383",
+        });
 
         // Create cake
-		let cake = new THREE.CylinderGeometry(0.27, 0.27, 0.35, 32, 32, false, 0, Math.PI*1.5);
-		this.cakeMesh = new THREE.Mesh(cake, cakeExteriorMaterial);
+        let cake = new THREE.CylinderGeometry(
+            0.27,
+            0.27,
+            0.35,
+            32,
+            32,
+            false,
+            0,
+            Math.PI * 1.5
+        );
+        this.cakeMesh = new THREE.Mesh(cake, cakeExteriorMaterial);
         this.cakeMesh.position.set(0, 0.18, 0);
 
         // Create cake interior
 
-        let cakeInterior = new THREE.PlaneGeometry(0.27, 0.35 );
-        this.cakeInteriorMesh = new THREE.Mesh(cakeInterior, cakeInteriorMaterial);
-        this.cakeInteriorMesh.rotateY(-Math.PI / 2)
-        this.cakeInteriorMesh.position.set(0, 0, 0.135)
+        let cakeInterior = new THREE.PlaneGeometry(0.27, 0.35);
+        this.cakeInteriorMesh = new THREE.Mesh(
+            cakeInterior,
+            cakeInteriorMaterial
+        );
+        this.cakeInteriorMesh.rotateY(-Math.PI / 2);
+        this.cakeInteriorMesh.position.set(0, 0, 0.135);
 
         const instance = this.cakeInteriorMesh.clone();
-        instance.rotateY((Math.PI / 2))
-        instance.position.set(-0.135, 0, 0)
+        instance.rotateY(Math.PI / 2);
+        instance.position.set(-0.135, 0, 0);
 
-        this.cakeMesh.add(this.cakeInteriorMesh)
-        this.cakeMesh.add(instance)
+        this.cakeMesh.add(this.cakeInteriorMesh);
+        this.cakeMesh.add(instance);
         this.cakeStandPlateMesh.add(this.cakeMesh);
     }
 
     buildCandle() {
-        
-        let candleMaterial = new THREE.MeshBasicMaterial({ color: "#f5d20c" })
-        let flameMaterial = new THREE.MeshBasicMaterial({ color: "#de7009" })
-        
+        let candleMaterial = new THREE.MeshBasicMaterial({ color: "#f5d20c" });
+        let flameMaterial = new THREE.MeshBasicMaterial({ color: "#de7009" });
+
         // Create candle
-		let candle = new THREE.CylinderGeometry(0.01, 0.01, 0.1);
-		this.candleMesh = new THREE.Mesh(candle, candleMaterial);
+        let candle = new THREE.CylinderGeometry(0.01, 0.01, 0.1);
+        this.candleMesh = new THREE.Mesh(candle, candleMaterial);
         this.candleMesh.position.set(0, 0.225, 0);
 
         // Create Flame
 
-        let flame = new THREE.ConeGeometry(0.01, 0.015)
-        this.flameMesh = new THREE.Mesh(flame, flameMaterial)
+        let flame = new THREE.ConeGeometry(0.01, 0.015);
+        this.flameMesh = new THREE.Mesh(flame, flameMaterial);
         this.flameMesh.position.set(0, 0.063, 0);
-        
-        
-        
+
         this.candleMesh.add(this.flameMesh);
-        this.cakeMesh.add(this.candleMesh)
+        this.cakeMesh.add(this.candleMesh);
     }
-    
+
     /**
      * updates the diffuse plane color and the material
-     * @param {THREE.Color} value 
+     * @param {THREE.Color} value
      */
     updateDiffusePlaneColor(value) {
-        this.diffusePlaneColor = value
-        this.planeMaterial.color.set(this.diffusePlaneColor)
+        this.diffusePlaneColor = value;
+        this.planeMaterial.color.set(this.diffusePlaneColor);
     }
     /**
      * updates the specular plane color and the material
-     * @param {THREE.Color} value 
+     * @param {THREE.Color} value
      */
     updateSpecularPlaneColor(value) {
-        this.specularPlaneColor = value
-        this.planeMaterial.specular.set(this.specularPlaneColor)
+        this.specularPlaneColor = value;
+        this.planeMaterial.specular.set(this.specularPlaneColor);
     }
     /**
      * updates the plane shininess and the material
-     * @param {number} value 
+     * @param {number} value
      */
     updatePlaneShininess(value) {
-        this.planeShininess = value
-        this.planeMaterial.shininess = this.planeShininess
+        this.planeShininess = value;
+        this.planeMaterial.shininess = this.planeShininess;
     }
-    
+
     /**
      * rebuilds the box mesh if required
      * this method is called from the gui interface
      */
     rebuildBox() {
         // remove boxMesh if exists
-        if (this.boxMesh !== undefined && this.boxMesh !== null) {  
-            this.app.scene.remove(this.boxMesh)
+        if (this.boxMesh !== undefined && this.boxMesh !== null) {
+            this.app.scene.remove(this.boxMesh);
         }
         this.buildBox();
-        this.lastBoxEnabled = null
+        this.lastBoxEnabled = null;
     }
-    
+
     /**
      * updates the box mesh if required
      * this method is called from the render method of the app
@@ -368,12 +393,11 @@ class MyContents  {
      */
     updateBoxIfRequired() {
         if (this.boxEnabled !== this.lastBoxEnabled) {
-            this.lastBoxEnabled = this.boxEnabled
+            this.lastBoxEnabled = this.boxEnabled;
             if (this.boxEnabled) {
-                this.app.scene.add(this.boxMesh)
-            }
-            else {
-                this.app.scene.remove(this.boxMesh)
+                this.app.scene.add(this.boxMesh);
+            } else {
+                this.app.scene.remove(this.boxMesh);
             }
         }
     }
@@ -381,7 +405,7 @@ class MyContents  {
     /**
      * updates the contents
      * this method is called from the render method of the app
-     * 
+     *
      */
     update() {
         /*// check if box mesh needs to be updated
@@ -391,9 +415,7 @@ class MyContents  {
         this.boxMesh.position.x = this.boxDisplacement.x
         this.boxMesh.position.y = this.boxDisplacement.y
         this.boxMesh.position.z = this.boxDisplacement.z*/
-        
     }
-
 }
 
 export { MyContents };
