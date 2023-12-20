1976ec837d05b0a5582d8fe3ffaab9404b05695e
up202007928 <up202007928@edu.fe.up.pt>
2023-12-20 13:19:00 +0000

    trackFaces seem to be working

--- tp3/MyTrack.js
+++ tp3/MyTrack.js
@@ -35,15 +35,12 @@ class MyTrack {
 		return lineObj;
 	}
 
-	buildTrackLine() {
+	buildInnerTrackLine() {
 		let vectors = this.data.track.points.map((point) => {
 			return new THREE.Vector3(...point.value);
 		});
 
 		const trackLine = this.initCatmullRomCurve(vectors);
-
-		/*const center = this.getCenterPoint(trackLine);
-		trackLine.position.sub(center);*/
 
 		this.app.scene.add(trackLine);
 	}
@@ -79,15 +76,13 @@ class MyTrack {
 	buildTrackFaces() {
 		const curve = this.trackCurve;
 
-		const pointsCount = 100,
-			pointsCount1 = 101;
+		const pointsCount = 100;
 		let pts = curve.getPoints(pointsCount);
 		let pts2 = curve.getPoints(pointsCount + 1);
 
 		const perpendiculars = this.getPerpendiculars(pts2);
 
 		const width = 2;
-		const widthSteps = 1;
 		const outerPoints = [];
 
 		for (let i = 0; i < pts.length; i++) {
@@ -109,9 +104,48 @@ class MyTrack {
 		const lineObj = new THREE.Mesh(tubeGeometry, this.lineMaterial);
 
 		this.app.scene.add(lineObj);
-
 		////////////////
 
+		let vertices = [];
+		let indices = [];
+
+		let addedPoints = 0;
+
+		for (let i = 0, nextIndex = i + 1; i < pts.length; i++, nextIndex++) {
+
+			if (i === pts.length - 1) {
+				nextIndex = 0
+			}
+
+			const pointIn = pts[i],
+				pointOut = outerPoints[i],
+				nextInPoint = pts[nextIndex],
+				nextOutPoint = outerPoints[nextIndex];
+
+			vertices.push(...nextInPoint);
+			vertices.push(...nextOutPoint);
+
+			vertices.push(...pointIn);
+			vertices.push(...pointOut);
+
+			indices.push(addedPoints, addedPoints + 2, addedPoints + 3);
+			indices.push(addedPoints + 3, addedPoints + 1, addedPoints);
+
+			addedPoints += 4;
+		}
+
+		const trackGeometry = new THREE.BufferGeometry();
+
+		trackGeometry.setIndex(indices);
+		trackGeometry.setAttribute(
+			"position",
+			new THREE.BufferAttribute(new Float32Array(vertices), 3)
+		);
+
+		const trackMaterial = new THREE.MeshBasicMaterial({ color: 0x222222 });
+		const trackMesh = new THREE.Mesh(trackGeometry, trackMaterial);
+
+		this.app.scene.add(trackMesh);
 	}
 }
 
